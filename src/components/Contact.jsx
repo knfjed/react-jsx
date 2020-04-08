33eba5559a4e09f9e76ebc0a2f86ce3e92caadb5
knfjed <knfjed@gmail.com>
2020-04-08 11:25:07 +0900

    fix contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,11 +17,51 @@ function Title() {
 
 function Form() {
   return (
-    <form method="post">
-      <label for="engage">
-        <input id="engage" type="text" name="yourName" />
-        <input type="submit" value="submit" />
-      </label>
+    <form className="contactForm" method="post">
+      <table>
+        <tbody>
+          <tr>
+            <td for="name">NAME</td>
+            <td>
+              <input id="name" type="text" name="name" placeholder="山田太郎" />
+            </td>
+          </tr>
+          <tr>
+            <td for="mail">MAIL</td>
+            <td>
+              <input
+                id="mail"
+                type="mail"
+                name="mail"
+                placeholder="exsample@gmail.com"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td for="tel">TEL</td>
+            <td>
+              <input
+                id="tel"
+                type="number"
+                name="tel"
+                placeholder="000-0000-0000"
+              ></input>
+            </td>
+          </tr>
+          <tr>
+            <td for="text">TEXT</td>
+            <td>
+              <input
+                id="text"
+                type="text"
+                name="text"
+                placeholder="自由にコメントをご記入ください"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <input type="submit" className="btn-gradient-radius" />
     </form>
   );
 }
